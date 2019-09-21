a7679f44d7b0cb1379fb2186dab4fd476ed25d02
Quinn Casey <qcasey1@gmail.com>
2019-09-21 13:31:19 -0700

    adding main functions

--- drok.go
+++ drok.go
@@ -3,11 +3,13 @@ package drok
 import (
 	"bufio"
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/tarm/serial"
 )
 
-// readSerial will continuously pull data from incoming serial
+// readSerial will pull data from serial
 func readSerial(serialDevice *serial.Port) ([]byte, error) {
 	reader := bufio.NewReader(serialDevice)
 	msg, err := reader.ReadBytes('\n')
@@ -20,19 +22,95 @@ func readSerial(serialDevice *serial.Port) ([]byte, error) {
 }
 
 // writeSerial pushes out a message to the open serial port
-func writeSerial(serialDevice *serial.Port, msg string) error {
+// will read response and return read value or write bool (as a float)
+func writeSerial(serialDevice *serial.Port, msg string) (float32, error) {
 	if serialDevice == nil {
-		return fmt.Errorf("Serial port is nil")
+		return 0, fmt.Errorf("Serial port is nil")
 	}
 
 	if len(msg) == 0 {
-		return fmt.Errorf("Cannot send an empty message")
+		return 0, fmt.Errorf("Cannot send an empty message")
 	}
 
 	_, err := serialDevice.Write([]byte(msg))
 	if err != nil {
-		return err
+		return 0, err
 	}
 
-	return nil
+	// Immediately read, to get either the OK resonse or value
+	output, err := readSerial(serialDevice)
+	if err != nil {
+		return 0, err
+	}
+
+	return parseOutput(output)
+}
+
+// parseOutput given a serial response in bytes, will parse based on expected output for DROK
+// This has been tested on a DKP6012, I've read different models use identical controls, but if
+// you're seeing an issue it's likely here.
+func parseOutput(data []byte) (float32, error) {
+	if len(data) == 0 {
+		return 0, fmt.Errorf("Empty output, cannot parse")
+	}
+
+	// Ensure the returned response is valid
+	response := string(data)
+	responseLength := len(response)
+	if response[0] != '#' {
+		return 0, fmt.Errorf("Unknown response. Recieved: %s", response)
+	}
+	if len(response) != 5 && len(response) != 14 {
+		return 0, fmt.Errorf("Unknown response length of %d (expected 14 for read or 5 for write). Recieved: %s", len(response), response)
+	}
+
+	if response[1] == 'w' {
+		// Verify write
+		if response[len(response)-2:] == "ok" {
+			return 1, nil
+		}
+		return 0, fmt.Errorf("Could not verify write, Recieved: %s", response)
+	} else if response[1] == 'r' {
+		// Get response of read, last 11 bits
+		floatVal, err := strconv.ParseFloat(response[responseLength-11:], 32)
+		if err != nil {
+			return 0, err
+		}
+
+		if response[2] == 'o' {
+			// Parse output response
+			return float32(floatVal), nil
+		} else if response[2] == 'i' || response[2] == 'u' {
+			// Parse current / voltage responses
+			// Returns 5.23 from 00000000523
+			return float32(floatVal) / 100, nil
+		}
+
+		// Unknown response type
+		return 0, fmt.Errorf("Unknown response type %b. Full response: %s", response[2], response)
+	}
+	return 0, nil
+}
+
+// Formats a float into a string, with these constrains:
+// Cut off to 2 decimals
+// Remove decimal from string
+// Cut off to 4 total digits
+func formatQuery(query float32) string {
+	//  Get in desired range & Cut off to 2 decimals
+	cf := fmt.Sprintf("%.2f", query*100)
+
+	// Remove decimal
+	cf = strings.Replace(cf, ".", "", -1)
+
+	// Ensure 4 digits
+	if len(cf) > 4 {
+		cf = fmt.Sprintf("%s", cf[len(cf)-4:])
+	} else if len(cf) < 4 {
+		// Append 0s until we have a len of 4
+		for len(cf) < 4 {
+			cf = fmt.Sprintf("0%s", cf)
+		}
+	}
+	return cf
 }
--- main.go
+++ main.go
@@ -9,34 +9,84 @@ https://benjames.io/2018/06/29/secret-uart-on-chinese-dcdc-converters/
 */
 package drok
 
-import "github.com/tarm/serial"
+import (
+	"fmt"
+
+	"github.com/tarm/serial"
+)
 
 // ReadVoltage will read the voltage with a resolution of 2 decimal places
 func ReadVoltage(device *serial.Port) (float32, error) {
-	return 0, nil
+	response, err := writeSerial(device, fmt.Sprintf("aru"))
+	if err != nil {
+		return 0, err
+	}
+
+	return response, nil
 }
 
 // ReadCurrent will read the current with a resolution of 2 decimal places
 func ReadCurrent(device *serial.Port) (float32, error) {
-	return 0, nil
+	response, err := writeSerial(device, fmt.Sprintf("ari"))
+	if err != nil {
+		return 0, err
+	}
+
+	return response, nil
 }
 
 // IsOn will return true if output is enabled
 func IsOn(device *serial.Port) (bool, error) {
-	return false, nil
+	response, err := writeSerial(device, fmt.Sprintf("aro"))
+	if err != nil {
+		return false, err
+	}
+
+	return response == 1, nil
 }
 
 // SetVoltage will set the voltage with a resolution of 2 decimal places
 func SetVoltage(device *serial.Port, value float32) error {
+	response, err := writeSerial(device, fmt.Sprintf("awu%s", formatQuery(value)))
+	if err != nil {
+		return err
+	}
+	if response != 1 {
+		return fmt.Errorf("Failed to verify write, response is: %f, expecting 1", response)
+	}
+
 	return nil
 }
 
 // SetCurrent will set the current with a resolution of 2 decimal places
 func SetCurrent(device *serial.Port, value float32) error {
+	response, err := writeSerial(device, fmt.Sprintf("awi%s", formatQuery(value)))
+	if err != nil {
+		return err
+	}
+	if response != 1 {
+		return fmt.Errorf("Failed to verify write, response is: %f, expecting 1", response)
+	}
+
 	return nil
 }
 
 // SetOutput will turn on or off power supply output
-func SetOutput(device *serial.Port, value bool) error {
+func SetOutput(device *serial.Port, output bool) error {
+	var query string
+	if output {
+		query = "awo1"
+	} else {
+		query = "awo0"
+	}
+
+	response, err := writeSerial(device, fmt.Sprintf(query))
+	if err != nil {
+		return err
+	}
+	if response != 1 {
+		return fmt.Errorf("Failed to verify write, response is: %f, expecting 1", response)
+	}
+
 	return nil
 }
